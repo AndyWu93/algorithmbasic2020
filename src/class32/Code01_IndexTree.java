acbf870638e9dd948ebd91d3cf7671b78440e9cd
jaysun197 <815533359@qq.com>
2022-01-11 21:19:39 +0800

    class32算法注释

--- src/class32/Code01_IndexTree.java
+++ src/class32/Code01_IndexTree.java
@@ -1,5 +1,31 @@
 package class32;
 
+/**
+ * index tree 树状数组
+ * 用来快速求出某个范围内的累加和。
+ * 范围内的累加和也可以用前缀数组快速求出，但是当涉及到元数据的更新后再求范围累加和，前缀数组就涉及到大量的更新，就不适用了
+ * 涉及单点更新、范围求和问题，线段树也可以做，但是index tree更优（线段树只针对单点更新，indexTree可以是二维的）
+ * index tree设计
+ * 将arr中的数，以小游戏2048的形式累加起来
+ * 元数组下标：       [1,2,3,4,5,6,7,8]
+ * indexTree累加后：sum[1],sum[1..2],sum[3],sum[1..4],sum[5],sum[5..6],sum[7],sum[1..8]
+ * 1. 对于任意i，有
+ * 假设indexTree[i],i的二进制为0 1011 1000，那indexTree[i]=sum[x..i],x的二进制为0 1011 0001，即i最右的1变成0后再加1
+ * 2. 求sum[1..i]
+ * sum[1..i]=indexTree[i]+indexTree[i1=i最右侧的1变成0]+indexTree[i2=i1最右侧的1变成0]+...+indexTree[in只剩1个1了]
+ * 举例求sum[1..01 0110 0110]
+ * = it[01 0110 0110] + it[01 0110 0100] + it[01 0110 0000] +...+ it[01 0000 0000]
+ * 为什么？将这些数按照第1点的规则拆开，就能看到一共是sum[1..01 0110 0110]
+ * 3. 当arr[1001 1000]的值变了，
+ * it[1001 1000]一定变了，还要哪些位置要变？
+ * i + 最右侧的1 得到i1，即1001 1000 + 0000 1000 -> it[1010 0000]
+ * i1 + 最右侧的1 得到i2，即1010 0000 + 0010 0000 -> it[1100 0000]
+ * ...
+ * 直到in越界
+ *
+ * 以上方法复杂度？
+ * index的位数决定复杂度，位数是多少？logN
+ */
 public class Code01_IndexTree {
 
 	// 下标从1开始！
@@ -14,22 +40,33 @@ public class Code01_IndexTree {
 			tree = new int[N + 1];
 		}
 
-		// 1~index 累加和是多少？
+		/**
+		 * sum方法
+		 * 1~index 累加和是多少？
+		 */
 		public int sum(int index) {
 			int ret = 0;
 			while (index > 0) {
+				/*index位置先加上去*/
 				ret += tree[index];
+				/*sum是多个位置的和，位置如何到，减去最右侧的1*/
 				index -= index & -index;
 			}
 			return ret;
 		}
 
+		/**
+		 * 在arr的index位置增加值d
+		 * indexTree中受影响的位置都要改变
+		 */
 		// index & -index : 提取出index最右侧的1出来
 		// index :           0011001000
 		// index & -index :  0000001000
 		public void add(int index, int d) {
 			while (index <= N) {
+				/*现在该位置加上d*/
 				tree[index] += d;
+				/*受影响的位置如何得到：index加上最右侧的1，直到越界*/
 				index += index & -index;
 			}
 		}
--- src/class32/Code04_AC2.java
+++ src/class32/Code04_AC2.java
@@ -5,6 +5,29 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
+/**
+ * 建立敏感词库str[],对于文本text，查询text有多少个存在于str[]中的敏感词，并返回个数
+ * 解决方案：AC自动机
+ * 设计思路：在前缀树的节点中添加fail指针。fail指针的设计思路来源于KMP
+ * fail指针逻辑：
+ * 1）头节点的fail=null
+ * 2）头节点的直接孩子的fail指向头节点
+ * 3）对于其他任意节点x
+ * 	寻找x的父节点y，假设y节点到x节点的路是1。
+ * 	查看y节点的fail，假设其指向z节点，查看z节点有没有指向1的路
+ * 		a. 有，通过1方向指向了k节点，那x的fail指向k
+ * 		b. 没有，就继续查看z的fail节点，直到找到了某个节点的fail节点，有1方向的路，x的fail直接指过去
+ * 		c. 如果最终fail指向null了都没有发现符合条件的节点，那x的fail指向头节点
+ * fail指针的实质：
+ * 	标记出其他所有的字符串中，哪个字符串的前缀和当前字符串的后缀相同，且匹配的长度最长
+ * 提问：为什么fail需要指向匹配长度最长的str？
+ * 	因为当该字符串匹配失败后，跳向下一个str，需要确保此时跳向的str拥有最长的已匹配前缀，
+ * 	如果该str依旧匹配失败，再跳向str拥有最长的已匹配前缀
+ * 	以此来保证，不会错过任何一个可能匹配成功的str
+ * 提问：为什么头节点直接孩子的fail指向头？
+ *  如果第二个字符匹配失败了，下个字符串匹配只能从头开始
+ *
+ */
 public class Code04_AC2 {
 
 	// 前缀树的节点
@@ -27,12 +50,14 @@ public class Code04_AC2 {
 	}
 
 	public static class ACAutomation {
+		/*头节点，前缀树代码*/
 		private Node root;
 
 		public ACAutomation() {
 			root = new Node();
 		}
 
+		/*前缀树建树*/
 		public void insert(String s) {
 			char[] str = s.toCharArray();
 			Node cur = root;
@@ -47,55 +72,83 @@ public class Code04_AC2 {
 			cur.end = s;
 		}
 
+		/*设置fail指针*/
 		public void build() {
+			/*用于宽度优先遍历，通过宽度优先来设置*/
 			Queue<Node> queue = new LinkedList<>();
 			queue.add(root);
 			Node cur = null;
 			Node cfail = null;
 			while (!queue.isEmpty()) {
-				// 某个父亲，cur
+				/*
+				* 此时弹出了某个节点，此时来设置它孩子的fail指针
+				* 因为前缀树不能找到父亲，只能找到孩子
+				* */
 				cur = queue.poll();
 				for (int i = 0; i < 26; i++) { // 所有的路
 					// cur -> 父亲  i号儿子，必须把i号儿子的fail指针设置好！
-					if (cur.nexts[i] != null) { // 如果真的有i号儿子
+					if (cur.nexts[i] != null) {
+						/*如果真的有i号儿子，先把儿子的fail指向root，因为如果最终没有地方指了一定会指向root*/
 						cur.nexts[i].fail = root;
+						/*拿到自己的fail，方便设置儿子的fail*/
 						cfail = cur.fail;
 						while (cfail != null) {
 							if (cfail.nexts[i] != null) {
+								/*
+								* 自己的fail不为空，且自己fail有i方向的路，那儿子的fail就是自己的fail在i方向的路上的节点
+								* 这里，最长前缀是由自己的fail指向的是最长的，所以儿子天然最长
+								* */
 								cur.nexts[i].fail = cfail.nexts[i];
+								/*找到了就结束*/
 								break;
 							}
+							/*没找到？自己的fail再跳一下，看下个fail*/
 							cfail = cfail.fail;
 						}
+						/*继续遍历*/
 						queue.add(cur.nexts[i]);
 					}
 				}
 			}
 		}
 
-		// 大文章：content
+		/*content查询敏感词*/
 		public List<String> containWords(String content) {
 			char[] str = content.toCharArray();
 			Node cur = root;
 			Node follow = null;
 			int index = 0;
 			List<String> ans = new ArrayList<>();
+			/*
+			* 遍历content
+			* 整体逻辑概述，维护一个变量cur，随着content的遍历，
+			* cur也尽可能的往下走，只要有路就走。
+			* 且每走一步都会看一眼此时匹配的前缀中，有没有已经成串的，收集一下并标记（下次收集的时候看到标记直接跳过）
+			* */
 			for (int i = 0; i < str.length; i++) {
 				index = str[i] - 'a'; // 路
-				// 如果当前字符在这条路上没配出来，就随着fail方向走向下条路径
+				/*
+				* 当前cur所在str已经没有路了，而且cur又不是头节点，那就随着fail方向看能不能继续走下去
+				* 跳尽了所有的fail，都没找到index反向的路，此时cur来到root节点，那就不要跳了
+				* */
 				while (cur.nexts[index] == null && cur != root) {
 					cur = cur.fail;
 				}
 				// 1) 现在来到的路径，是可以继续匹配的
 				// 2) 现在来到的节点，就是前缀树的根节点
+				/*是否有index方向的路？有的话就过去，没有就直接回到root*/
 				cur = cur.nexts[index] != null ? cur.nexts[index] : root;
+				/*此时，停下来，沿着cur的fail逛一圈，看有没有已经成串的*/
 				follow = cur;
+				/*一路fail直到root节点*/
 				while (follow != root) {
+					/*之前逛过，直接停，可能是中间经历了一次回到root，接下来Content中又出现了同样的敏感词*/
 					if (follow.endUse) {
 						break;
 					}
 					// 不同的需求，在这一段之间修改
 					if (follow.end != null) {
+						/*成串的，收集一下，再打标*/
 						ans.add(follow.end);
 						follow.endUse = true;
 					}
