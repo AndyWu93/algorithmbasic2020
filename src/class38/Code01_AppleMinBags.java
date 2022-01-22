3bcc01ca7e70b9019092189357da0929ba0e3985
jaysun197 <815533359@qq.com>
2022-01-22 16:53:46 +0800

    有序表3注释

--- src/class38/Code01_AppleMinBags.java
+++ src/class38/Code01_AppleMinBags.java
@@ -1,7 +1,21 @@
 package class38;
 
+/**
+ * 解题技巧之计数器找规律
+ * 通过暴力解得到答案，再分析入参和出参之间的规律
+ *
+ * 小虎去买苹果，商店只提供两种类型的塑料袋，每种类型都有任意数量。
+ * 1）能装下6个苹果的袋子
+ * 2）能装下8个苹果的袋子
+ * 小虎可以自由使用两种袋子来装苹果，但是小虎有强迫症，他要求自己使用的袋子数量必须最少，且使用的每个袋子必须装满。
+ * 给定一个正整数N，返回至少使用多少袋子。如果N无法让使用的每个袋子必须装满，返回-1
+ *
+ */
 public class Code01_AppleMinBags {
 
+	/**
+	 * 暴力解
+	 */
 	public static int minBags(int apple) {
 		if (apple < 0) {
 			return -1;
@@ -20,6 +34,10 @@ public class Code01_AppleMinBags {
 		return -1;
 	}
 
+	/**
+	 * 找到规律以后
+	 *
+	 */
 	public static int minBagAwesome(int apple) {
 		if ((apple & 1) != 0) { // 如果是奇数，返回-1
 			return -1;
